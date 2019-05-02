1f0d319f6c27af8122e820660759db7275c2e0e1
Willy Nugraha <willymiiee@gmail.com>
2019-05-02 23:49:45 +0800

    Add script to generate JWT secret key

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 curl -sS https://getcomposer.org/installer | php \
  && php composer.phar install \
  && cp .env.example .env \
- && php artisan key:generate
+ && php artisan key:generate \
+ && php artisan jwt:secret
